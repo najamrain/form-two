eb79b155754c5a0f1c9c18f21f94fa1e7884ad24
Najam Rain <najamrain@gmail.com>
2020-05-03 00:22:08 +0400

    Java file added

---
+++ java.js
@@ -0,0 +1,21 @@
+window.sr = ScrollReveal();
+sr.reveal(".heading-container", {
+  duration: 2000,
+  origin: "left",
+  distance: "500px",
+});
+sr.reveal(".inputBox", {
+  duration: 2000,
+  origin: "left",
+  distance: "200px",
+});
+sr.reveal(".help", {
+  duration: 2000,
+  origin: "top",
+  distance: "200px",
+});
+sr.reveal(".submiit", {
+  duration: 2000,
+  origin: "top",
+  distance: "200px",
+});
